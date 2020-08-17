777ad6a749aeee8c484ea1d67d5e463ac693719e
Juan Chacon-Hurtado <j.chaconhurtado@tudelft.nl>
2020-08-17 16:35:12 +0200

    Update setup.py
    modified description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
       name='decisiorama',
       version='0.1',
-      description='A libraryfor preference elicitacion in MCDA',
+      description='Decisi-o-rama: An open-source Python library for multi-attribute value/utility decision analysis',
       author='Juan Carlos Chacon-Hurtado',
       author_email='j.chaconhurtado@tudelft.nl',
       url='None',
